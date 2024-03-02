5c426e99a6ceb4f33c9b9a9c9b67ce5e5042d736
Areej Mishal Mirani <amirani@eceprog6.ecn.purdue.edu>
2024-03-02 14:10:45 -0500

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
-print("init")
+import streamlit as st
+from pathlib import Path
+import base64
+
+# Initial page config
+
+st.set_page_config(
+    page_title = 'GradMove',
+    layout = "wide",
+    initial_sidebar_state = "expanded",
+)
+
