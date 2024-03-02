84c10c09aba003bae25df9c8bc82610869d7c79b
shreya <laxminag@purdue.edu>
2024-03-02 16:06:42 -0500

    added pink line to housingDataFunction.py

--- housingDataFunction.py
+++ housingDataFunction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.ticker as ticker
 
 def plot_housing_prices_for_city(city):
     # read csv file into pandas dataframe
@@ -14,11 +15,13 @@ def plot_housing_prices_for_city(city):
 
     x_values = housing_cities.columns[6:]
     y_values = housing_cities[housing_cities['Metro'] == city].iloc[0, 6:]
-    plt.plot(x_values, y_values, label=city, color = 'pink', linewidth = 2.5, marker = 'o')
+    plt.plot(x_values, y_values, label=city, color = 'pink', linewidth = 2.5)
     plt.xlabel('Year-Month')
     plt.ylabel('Price')
     plt.title('Median Rental Price 1 Bedroom')
-    plt.xticks(rotation=90)
+    plt.tick_params(axis='x', which='major', labelsize=8)
+    plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(nbins=10))
+    plt.xticks(rotation=45)
     plt.legend()
     plt.show()
 
