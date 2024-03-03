91a814cbaa0ce1e3a93a1f1dfb5fd346da27df13
shreya <laxminag@purdue.edu>
2024-03-02 23:56:22 -0500

    changed absolute paths and added all bedrooms

--- app_with_chatbot.py
+++ app_with_chatbot.py
@@ -3,6 +3,9 @@ import pandas as pd
 import openai
 import langchain
 import toml
+import housingDataFunction
+import matplotlib.pyplot as plt
+
 
 # Define your OpenAI API Key
 secrets = toml.load(".streamlit/secrets.toml")
@@ -13,6 +16,16 @@ OPENAI_API_KEY = secrets['openai']['api_key']
 # [openai]
 # api_key =
 
+# Load the config settings from config.toml
+config = toml.load('.streamlit/config.toml')
+# Get the theme settings
+theme_settings = config.get('theme', {})
+base_theme = theme_settings.get('base', 'light')
+primary_color = theme_settings.get('primaryColor', '#AA336A')
+background_color = theme_settings.get('backgroundColor', '#000000')
+secondary_background_color = theme_settings.get('secondaryBackgroundColor', '#f0f2f6')
+text_color = theme_settings.get('textColor', '#AA336A')
+font = theme_settings.get('font', 'serif')
 
 def page_config():
     """Configures the initial page settings."""
@@ -37,7 +50,11 @@ def main_body():
     st.markdown("Your go-to app for finding housing near your dream job, curated based on tastes in housing, price, transport, and access to healthcare.")
     
     df = pd.DataFrame({
-        'cities': ["San Francisco", "Chicago", "New York", "Seattle", "Cambridge", "Amsterdam"],
+        # add all the cities in the United States
+        'cities': ["San Francisco", "Chicago", "New York", "Seattle", "Cambridge", "Boston", "Los Angeles", "Austin", "Denver", "Portland", "Atlanta", "Dallas", "Philadelphia", "Kansas City",
+                   "Houston", "San Antonio", "Philadelphia", "Phoenix", "Las Vegas", "San Diego", 
+                   "Dallas", "Austin", "San Jose", "Jacksonville", "Indianapolis", "Charlotte",
+                   "Louisville", "Sacramento",'Baltimore'],
     })
     
     option = st.selectbox(
@@ -46,6 +63,23 @@ def main_body():
     )
     
     st.write('You selected:', option)
+    st.write("Excited to move to", option, "and start your new job?")
+    st.write("Let's get started!")
+
+    st.subheader("Median Rent Prices for 1-Bedroom Apartments")
+    # housingDataFunction.py has the function to plot the median 
+    # rent prices for 1-bedroom apartments through the years
+    for city in ['Atlanta-Sandy Springs-Roswell', 'Dallas-Fort Worth-Arlington', 'Chicago-Naperville-Elgin', 'New York-Newark-Jersey City', 'Kansas City', 
+          'Philadelphia-Camden-Wilmington', 'Denver-Aurora-Lakewood', 'Seattle-Tacoma-Bellevue', 'Boston-Cambridge-Newton', 'San Francisco-Oakland-Hayward']:
+        if (option.split(","))[0] in city:
+            st.write(city)
+            
+            img = housingDataFunction.plot_housing_prices_for_city(city)
+            # display png image that it saves
+            st.image(img)
+            break
+
+    
 
 def handle_chat_input(user_input, use_langchain=False):
     """Handles the chat input, querying OpenAI or LangChain."""
--- housingDataFunction.py
+++ housingDataFunction.py
@@ -5,28 +5,40 @@ import matplotlib.ticker as ticker
 
 def plot_housing_prices_for_city(city):
     # read csv file into pandas dataframe
-    housing = pd.read_csv('/Users/shreya/Purdue/InnovateHer/GradMove/GradMove/archive/City_MedianRentalPrice_1Bedroom.csv')
+    housing = pd.read_csv('/archive/City_MedianRentalPrice_1Bedroom.csv')
+    housing2 = pd.read_csv('/archive/City_MedianRentalPrice_2Bedroom.csv')
+    housing3 = pd.read_csv('/archive/City_MedianRentalPrice_3Bedroom.csv')
+    housing4 = pd.read_csv('/archive/City_MedianRentalPrice_4Bedroom.csv')
 
-    housing_cities = housing[housing['Metro'] == city]
-    if housing_cities.shape[0] > 1:
-        for i in range(6, len(housing_cities.columns)):
-            housing_cities.loc[housing_cities['Metro'] == city, housing_cities.columns[i]] = housing_cities[housing_cities['Metro'] == city].iloc[:, i].mean()
-            housing_cities = housing_cities.drop_duplicates(subset='Metro', keep='first')
+    i = 1
+    list_images = []
+    for h in [housing, housing2, housing3, housing4]:
+        housing_cities = h[h['Metro'] == city]
+        if housing_cities.shape[0] > 1:
+            for i in range(6, len(housing_cities.columns)):
+                housing_cities.loc[housing_cities['Metro'] == city, housing_cities.columns[i]] = housing_cities[housing_cities['Metro'] == city].iloc[:, i].mean()
+                housing_cities = housing_cities.drop_duplicates(subset='Metro', keep='first')
 
-    x_values = housing_cities.columns[6:]
-    y_values = housing_cities[housing_cities['Metro'] == city].iloc[0, 6:]
-    plt.plot(x_values, y_values, label=city, color = 'pink', linewidth = 2.5)
-    plt.xlabel('Year-Month')
-    plt.ylabel('Price')
-    plt.title('Median Rental Price 1 Bedroom')
-    plt.tick_params(axis='x', which='major', labelsize=8)
-    plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(nbins=10))
-    plt.xticks(rotation=45)
-    plt.legend()
-    plt.show()
+        x_values = housing_cities.columns[6:]
+        y_values = housing_cities[housing_cities['Metro'] == city].iloc[0, 6:]
+        # y_values if giving me index out of bounds error, why
+        # I think it's because the data is not being read in properly
+        # I need to check the data types of the columns
 
-    # save this into a new csv
-    housing_cities.to_csv('/Users/shreya/Purdue/InnovateHer/GradMove/GradMove/City_MedianRentalPrice_1Bedroom_Cities_filtered.csv', index=False)    
+        plt.plot(x_values, y_values, label=city, color = 'pink', linewidth = 2.5)
+        plt.xlabel('Year-Month')
+        plt.ylabel('Price')
+        plt.title(f'Median Rental Price {i} Bedroom')
+        plt.tick_params(axis='x', which='major', labelsize=8)
+        plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(nbins=10))
+        plt.xticks(rotation=45)
+        plt.legend()
+        # save plot to png
+        plt.savefig(f'/City_MedianRentalPrice_{i}Bedroom_Cities_filtered.png')
+        list_images.append(f'City_MedianRentalPrice_{i}Bedroom_Cities_filtered.png')    
+        i += 1
+    return list_images
+       
     
 # Example usage
 plot_housing_prices_for_city("New York-Newark-Jersey City")
