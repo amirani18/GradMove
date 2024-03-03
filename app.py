b6ac4048351e796f0127dfd10db6016c4c82ddf0
amirani18 <112004077+amirani18@users.noreply.github.com>
2024-03-02 22:56:30 -0500

    i

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def page1():
         with col2:
             st.subheader('Image in Column 2')
             st.image(chart, caption='cost of providers in state')
-            
+
     def healthcare_access(option):
         input_city = option
         input_state = city_to_state.get(input_city)
@@ -134,6 +134,32 @@ def page1():
 def page2():
     st.header("GradMove 🎓")
     st.write("This is Chatbot.")
+    def example():
+        random_df = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+        my_grid = grid(2, [2, 4, 1], 1, 4, vertical_align="bottom")
+
+        # Row 1:
+        my_grid.dataframe(random_df, use_container_width=True)
+        my_grid.line_chart(random_df, use_container_width=True)
+        # Row 2:
+        my_grid.selectbox("Select Country", ["Germany", "Italy", "Japan", "USA"])
+        my_grid.text_input("Your name")
+        my_grid.button("Send", use_container_width=True)
+        # Row 3:
+        my_grid.text_area("Your message", height=40)
+        # Row 4:
+        my_grid.button("Example 1", use_container_width=True)
+        my_grid.button("Example 2", use_container_width=True)
+        my_grid.button("Example 3", use_container_width=True)
+        my_grid.button("Example 4", use_container_width=True)
+        # Row 5 (uses the spec from row 1):
+        with my_grid.expander("Show Filters", expanded=True):
+            st.slider("Filter by Age", 0, 100, 50)
+            st.slider("Filter by Height", 0.0, 2.0, 1.0)
+            st.slider("Filter by Weight", 0.0, 100.0, 50.0)
+        my_grid.dataframe(random_df, use_container_width=True)
+    example()
 
 # def page3():
 #     st.header("Page 3")
